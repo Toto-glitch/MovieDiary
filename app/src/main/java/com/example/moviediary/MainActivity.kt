07d5d8090ed4e8f9b6fd55ca51d1cb25569e3c08
Toto-glitch <sergeevanton03072007@gmail.com>
2026-09-07 14:13:45 +0900

    Add test data in screen

--- app/src/main/java/com/example/moviediary/MainActivity.kt
+++ app/src/main/java/com/example/moviediary/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.moviediary
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val movies = listOf(
+            Movie(1, "Начало", 2010, 8.8f),
+            Movie(2, "Интерстеллар", 2014, 8.6f),
+            Movie(3, "Матрица", 1999, 8.7f),
+            Movie(4, "Джентльмены", 2019, 8.3f),
+            Movie(5, "Довод", 2020, 7.4f)
+        )
+
+        val rvMovies = findViewById<RecyclerView>(R.id.rvMovie)
+        rvMovies.layoutManager = LinearLayoutManager(this)
+        rvMovies.adapter = MovieAdapter(movies)
     }
 }
