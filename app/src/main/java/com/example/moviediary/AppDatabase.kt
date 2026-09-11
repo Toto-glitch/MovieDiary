c57abd394c96ed79f5437d9ecadf492f3182ec54
Toto-glitch <sergeevanton03072007@gmail.com>
2026-09-11 09:15:03 +0900

    Create class for db connection

---
+++ app/src/main/java/com/example/moviediary/AppDatabase.kt
@@ -0,0 +1,45 @@
+package com.example.moviediary
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.moviediary.dao.GenreDao
+import com.example.moviediary.dao.MovieDao
+import com.example.moviediary.dao.MovieTagDao
+import com.example.moviediary.dao.TagDao
+import com.example.moviediary.models.Genre
+import com.example.moviediary.models.Movie
+import com.example.moviediary.models.MovieTagRef
+import com.example.moviediary.models.Tag
+import kotlin.concurrent.Volatile
+
+@Database(
+    entities = [Movie::class, Genre::class, Tag::class, MovieTagRef::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+    abstract fun genreDao(): GenreDao
+    abstract fun tagDao(): TagDao
+    abstract fun movieTagDao(): MovieTagDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "movie_diary"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
