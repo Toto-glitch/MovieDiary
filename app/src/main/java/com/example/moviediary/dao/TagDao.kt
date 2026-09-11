b3f1effc22f81c310fd802bf2208fc3cd0fbb79e
Toto-glitch <sergeevanton03072007@gmail.com>
2026-09-11 09:00:23 +0900

    Create Tag dao interface

---
+++ app/src/main/java/com/example/moviediary/dao/TagDao.kt
@@ -0,0 +1,17 @@
+package com.example.moviediary.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.moviediary.models.Tag
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TagDao {
+
+    @Insert
+    suspend fun insert(tag: Tag): Long
+
+    @Query("SELECT * FROM tags")
+    fun getAllTags(): Flow<List<Tag>>
+}
