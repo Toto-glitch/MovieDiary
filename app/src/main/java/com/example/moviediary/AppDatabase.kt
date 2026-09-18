b437c23ce7956296ca52a0ed38da3c7a36d1f229
Toto-glitch <sergeevanton03072007@gmail.com>
2026-09-18 22:35:05 +0900

    Add genres into db

--- app/src/main/java/com/example/moviediary/AppDatabase.kt
+++ app/src/main/java/com/example/moviediary/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.moviediary.dao.GenreDao
 import com.example.moviediary.dao.MovieDao
 import com.example.moviediary.dao.MovieTagDao
@@ -12,6 +13,9 @@ import com.example.moviediary.models.Genre
 import com.example.moviediary.models.Movie
 import com.example.moviediary.models.MovieTagRef
 import com.example.moviediary.models.Tag
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.concurrent.Volatile
 
 @Database(
@@ -36,7 +40,19 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "movie_diary"
-                ).build()
+                )
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val genreDao = getDatabase(context).genreDao()
+                                listOf("Драма", "Фантастика", "Криминал", "Комедия", "Триллер",
+                                    "Биография", "Анимация", "Ужасы", "История", "Романтика")
+                                    .forEach { genreDao.insert(Genre(name = it)) }
+                            }
+                        }
+                    })
+                    .build()
                 INSTANCE = instance
                 instance
             }
