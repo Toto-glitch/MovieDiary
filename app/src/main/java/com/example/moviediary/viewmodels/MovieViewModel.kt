8634c547a8c92bfffda631b8ecb424171613c715
Toto-glitch <sergeevanton03072007@gmail.com>
2026-09-19 16:38:10 +0900

    Add GenreDao and TagDao in MovieViewModel

--- app/src/main/java/com/example/moviediary/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/example/moviediary/viewmodels/MovieViewModel.kt
@@ -6,13 +6,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.moviediary.AppDatabase
+import com.example.moviediary.models.Genre
 import com.example.moviediary.models.Movie
 import kotlinx.coroutines.launch
 
 class MovieViewModel(app: Application) : AndroidViewModel(app) {
 
-    private val dao = AppDatabase.getDatabase(app).movieDao()
-    val allMovies: LiveData<List<Movie>> = dao.getAllMovies().asLiveData()
+    private val movieDao = AppDatabase.getDatabase(app).movieDao()
+    private val genreDao = AppDatabase.getDatabase(app).genreDao()
+    private val tagDao = AppDatabase.getDatabase(app).tagDao()
+    private val movieTagDao = AppDatabase.getDatabase(app).movieTagDao()
+    val allMovies: LiveData<List<Movie>> = movieDao.getAllMovies().asLiveData()
+    val allGenres: LiveData<List<Genre>> = genreDao.getAllGenres().asLiveData()
 
     fun insert(movie: Movie) = viewModelScope.launch {
         dao.insert(movie)
